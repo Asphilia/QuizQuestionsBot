d6bd6cc9c37c9631e863918eb3cf3618da39d1eb
Asphilia <janike@katter-ebe.de>
2023-02-09 21:48:45 +0100

    Some more abstractions

--- QuizQuestions.py
+++ QuizQuestions.py
@@ -3,13 +3,15 @@ from discord.ext import commands
 import random
 import os
 
+channel_id = os.getenv("CHANNEL") # The channel id is an environment variable
+
 description = """Hey! I'm your learning assistant for Computational Intelligence
             and Machine Learning I. I will ask you different questions regarding
              the topics in the course, and you can discuss the answers. I will
              not check your answers though, the Admins may do that ;)"""
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True # we want to read and write messages
 bot = commands.Bot(command_prefix=".", description=description, intents=intents)
 
 @bot.event
@@ -25,8 +27,7 @@ async def question(ctx):
     Type :question
     """
     #print("Question")
-    #channel = bot.get_channel(1073229394501447690) # Testchannel on my testserver
-    channel = bot.get_channel(1072079980235857951) # channel on the server, with the correct questions :)
+    channel = bot.get_channel(channel_id) # get channel with questions
     if channel:
         #print("Found channel...")
         messages = [message async for message in channel.history(limit=200)]
